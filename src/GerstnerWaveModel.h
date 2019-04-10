4444f9897259a411f334f0c7c7e79b8a9776931b
Anas Louter <anas.louter@ensimag.grenoble-inp.fr>
2019-04-10 09:25:47 +0000

    nique

---
+++ src/GerstnerWaveModel.h
@@ -0,0 +1,24 @@
+#ifndef GERSTNERWAVE_H
+#define GERSTNERWAVE_H
+
+#include <iomanip>
+#include <fstream>
+#include <iostream>
+#include <cstring>
+#include <ostream>
+#include <cstdlib>
+#include <ctime>
+
+#include <limits>
+using namespace std;
+
+class GerstnerWaveModel : public WaveModel
+{
+	public :
+
+
+	private:
+		
+};
+
+#endif
