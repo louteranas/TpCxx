e0e6c83558daa4ba68bd112c64842eaa31e0e2fb
Anas Louter <anas.louter@ensimag.grenoble-inp.fr>
2019-04-10 10:44:19 +0000

    classes finies tp3

--- src/GerstnerWaveModel.h
+++ src/GerstnerWaveModel.h
@@ -1,5 +1,5 @@
-#ifndef GERSTNERWAVE_H
-#define GERSTNERWAVE_H
+#ifndef GerstnerWaveModelMODEL_H
+#define GerstnerWaveModelMODEL_H
 
 #include <iomanip>
 #include <fstream>
@@ -12,13 +12,29 @@
 #include <limits>
 using namespace std;
 
-class GerstnerWaveModel : public WaveModel
+class GerstnerWaveModel
 {
 	public :
-
+		GerstnerWaveModel(double lx, double ly, int nx, int ny, const vector <GerstnerWave> waves);
+		~GerstnerWaveModel();
+		GerstnerWaveModel(const GerstnerWaveModel &waveModel);
+		GerstnerWaveModel &operator=(const GerstnerWaveModel &waveModel);
+		GerstnerWaveModel(GerstnerWaveModel &&waveModel);
+		GerstnerWaveModel &operator=(GerstnerWaveModel &&waveModel);
+		Height operator()(double t);
+		void add(GerstnerWave wave);
+		double getLx();
+		double getLy();
+		double getNx();
+		double getNy();
+		const vector <GerstnerWave> getWaves();
 
 	private:
-		
+		double lx;
+		double ly;
+		double nx;
+		double ny;
+		const vector <GerstnerWave> waves;
 };
 
 #endif
